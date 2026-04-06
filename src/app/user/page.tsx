6808a7b2a54a5ecc058b46d17b2a8ce915dd5e35
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-07 01:25:01 +0200

    remove Home component and inline its contents directly into Ecommerce page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
+import {EcommerceMetrics} from "@/components/ecommerce/EcommerceMetrics";
+import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
+import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
+import StatisticsChart from "@/components/ecommerce/StatisticsChart";
+import DemographicCard from "@/components/ecommerce/DemographicCard";
+import RecentOrders from "@/components/ecommerce/RecentOrders";
 import React from "react";
-import Home from '@/components/pages/Home'
 
 
 export const metadata: Metadata = {
@@ -10,5 +15,29 @@ export const metadata: Metadata = {
 };
 
 export default  function Ecommerce() {
-  return (<Home />);
+  return (
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <EcommerceMetrics />
+
+          <MonthlySalesChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <MonthlyTarget />
+        </div>
+
+        <div className="col-span-12">
+          <StatisticsChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <DemographicCard />
+        </div>
+
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div>
+      </div>
+  );
 }
