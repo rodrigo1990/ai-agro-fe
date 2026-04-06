a7690eff0d403d006c18f20d5d091441207f7c38
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-06 15:54:07 +0200

    refactor: extract Home component from Ecommerce page and move authentication logic to Home component

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,60 +1,14 @@
-'use client'
 import type { Metadata } from "next";
-import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
-import React, {useEffect} from "react";
-import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
-import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "@/components/ecommerce/StatisticsChart";
-import RecentOrders from "@/components/ecommerce/RecentOrders";
-import DemographicCard from "@/components/ecommerce/DemographicCard";
-import api from '@/lib/axios';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import Home from '@/components/pages/Home'
 
 
-// export const metadata: Metadata = {
-//   title:
-//     "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-//   description: "This is Next.js Home for TailAdmin Dashboard Template",
-// };
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
 
-export default function Ecommerce() {
-    const router = useRouter();
-    const isAuthenticated = async () => {
-        await api.get('/api/user').then(async response => {
-            console.log(response)
-        }).catch(error => {
-            console.log(error);
-            router.push('/');
-        })
-    }
-
-    useEffect(() => {
-        isAuthenticated()
-    })
-
-  return (
-    <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics />
-
-        <MonthlySalesChart />
-      </div>
-
-      <div className="col-span-12 xl:col-span-5">
-        <MonthlyTarget />
-      </div>
-
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div>
-
-      <div className="col-span-12 xl:col-span-5">
-        <DemographicCard />
-      </div>
-
-      <div className="col-span-12 xl:col-span-7">
-        <RecentOrders />
-      </div>
-    </div>
-  );
+export default  function Ecommerce() {
+  return (<Home />);
 }
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,46 @@
+import api from "@/lib/axios";
+import {EcommerceMetrics} from "@/components/ecommerce/EcommerceMetrics";
+import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
+import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
+import StatisticsChart from "@/components/ecommerce/StatisticsChart";
+import DemographicCard from "@/components/ecommerce/DemographicCard";
+import RecentOrders from "@/components/ecommerce/RecentOrders";
+import React from "react";
+
+export default async function Ecommerce() {
+    const isAuthenticated = async () => {
+        await api.get('/api/user').then(async response => {
+            console.log(response)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    isAuthenticated()
+
+    return (
+        <div className="grid grid-cols-12 gap-4 md:gap-6">
+            <div className="col-span-12 space-y-6 xl:col-span-7">
+                <EcommerceMetrics />
+
+                <MonthlySalesChart />
+            </div>
+
+            <div className="col-span-12 xl:col-span-5">
+                <MonthlyTarget />
+            </div>
+
+            <div className="col-span-12">
+                <StatisticsChart />
+            </div>
+
+            <div className="col-span-12 xl:col-span-5">
+                <DemographicCard />
+            </div>
+
+            <div className="col-span-12 xl:col-span-7">
+                <RecentOrders />
+            </div>
+        </div>
+    );
+}
