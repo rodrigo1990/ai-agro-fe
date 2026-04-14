0bdad6bd519f1d4796b7bd4eb79a7670d1d5dffe
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-15 00:23:25 +0200

    refactor: update proxy logic for root path redirection and matcher configuration

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,25 +4,32 @@ import {getSession} from "@/app/lib/sessions";
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
-    const cookieStore = request.cookies
+
+    let cookie = null
+
+    try{
+        cookie = await getSession()
+    }catch (e) {
+        return NextResponse.redirect(new URL('/auth/signin', request.url));
+    }
+
     if (request.nextUrl.pathname.startsWith('/user')) {
-        let cookie = null
-        try{
-            cookie = await getSession()
-        }catch (e) {
-            console.log(e)
-            return NextResponse.redirect(new URL('/auth/signin', request.url));
-        }
-        console.log('proxy user path: '+cookie.name)
-        console.log(cookieStore.get('session'))
+
         if (!cookie)
             return NextResponse.redirect(new URL('/auth/signin', request.url));
         else
-            return NextResponse.next()
+            return NextResponse.next();
+    }
+
+    if (request.nextUrl.pathname == '/') {
+        if (!cookie)
+            return NextResponse.redirect(new URL('/auth/signin', request.url));
+        else
+            return NextResponse.redirect(new URL('/user', request.url));
     }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/user/:path*', '/signout'],
+    matcher: ['/user/:path*', '/signout', '/'],
 }
