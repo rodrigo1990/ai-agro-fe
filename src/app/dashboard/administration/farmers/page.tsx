9ce24a418b7fe60913604f46a4f5a73275f79bf7
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-20 00:24:16 +0200

    refactor: replace farmers list with table layout, enhance visuals, and update "add products" button styling

--- src/app/dashboard/administration/farmers/page.tsx
+++ src/app/dashboard/administration/farmers/page.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import {getToken} from "@/app/lib/sessions";
 import Link from "next/link";
 import api from "@/lib/axios";
+import {Table, TableBody, TableCell, TableHeader, TableRow} from "@/components/ui/table";
+import Image from "next/image";
+import Badge from "@/components/ui/badge/Badge";
 
 export const metadata: Metadata = {
   title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
@@ -39,12 +42,55 @@ export default async function FormElements() {
             <p>Ups! Todavia no hay productores en el sistema, empezá a agregar aqui!</p>
           ) : (
               <div>
-                <ul>
-                  {farmers.map((farmer) => (
-                    <li key={farmer.id}>{farmer.name}</li>
-                  ))}
-                </ul>
-                <Link href="/dashboard/administration/farmers/detail">agregar productos</Link>
+                  <Table>
+                      {/* Table Header */}
+                      <TableHeader className="border-gray-100 dark:border-gray-800 border-y">
+                          <TableRow>
+                              <TableCell
+                                  isHeader
+                                  className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                              >
+                                  Name
+                              </TableCell>
+                              <TableCell
+                                  isHeader
+                                  className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                              >
+                                  Action
+                              </TableCell>
+                          </TableRow>
+                      </TableHeader>
+
+                      {/* Table Body */}
+
+                      <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
+                          {farmers.map((farmer) => (
+                              <TableRow key={farmer.id} className="">
+                                  <TableCell className="py-3">
+                                      <div className="flex items-center gap-3">
+                                          <div>
+                                              <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                                  {farmer.name}
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </TableCell>
+                                  <TableCell className="py-3">
+                                      <div className="flex items-center gap-3">
+                                          <div>
+                                              <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                                  Editar
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </TableCell>
+                              </TableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
+                <Link
+                    href="/dashboard/administration/farmers/detail"
+                    className="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300 px-5 py-3.5 text-sm bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300 ">agregar productos</Link>
               </div>
           )}
 
