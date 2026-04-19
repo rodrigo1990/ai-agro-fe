464846ec56f770fe72b5dc98fbbbf5a1567d2994
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-20 00:28:43 +0200

    refactor: update "add products" button text to "Agregar Productor" in farmers page for clarity

--- src/app/dashboard/administration/farmers/page.tsx
+++ src/app/dashboard/administration/farmers/page.tsx
@@ -90,7 +90,9 @@ export default async function FormElements() {
                   </Table>
                 <Link
                     href="/dashboard/administration/farmers/detail"
-                    className="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300 px-5 py-3.5 text-sm bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300 ">agregar productos</Link>
+                    className="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300 px-5 py-3.5 text-sm bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300 ">
+                    Agregar Productor
+                </Link>
               </div>
           )}
 
